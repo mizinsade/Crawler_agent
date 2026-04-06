273f33885040a2c7cb7165e544560cb655b89c8e
mizinsade <mizinsade@gmail.com>
2026-04-07 01:20:51 +0900

    메모리 최적화를 위한 multiprocessing Pool 적용 및 속도 증가를 위한 curl 사용

--- Crawler3.py
+++ Crawler3.py
@@ -18,23 +18,25 @@ import signal
 import subprocess
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 from queue import Empty
-sys.path.append("/home/sm/Documents/")
+sys.path.append("/home/mizin/")
 from NexusDB.NexusCore import NexusCore
 
 URL_FIND_PROCESS_WORKER = 5
-CONTETN_EXTRACT_PROCESS_WORKER = 80
-DATABASE_DIR = "/media/sm/T31/llm_info_db2"
-CHECKPOINT_FILE = os.path.join("/media/sm/T31/", "crawler_checkpoint2.json")
+CONTETN_EXTRACT_PROCESS_WORKER = 2
+DATABASE_DIR = "/home/mizin/llm_info_db4"
+CHECKPOINT_FILE = os.path.join(DATABASE_DIR, "crawler_checkpoint2.json")
 START_URL = ["https://news.naver.com/", "https://www.bbc.com/news", "https://www.ft.com/world"]
 
+MAXWORKPERCHILD = 100000
 
 # --- 1번 프로세스: DB 저장 및 통계 출력 ---
 def db_saver_process(data_queue, stop_event):
     print("[Process 1] DB Saver 가동 중...")
-    core = NexusCore(base_dir=DATABASE_DIR)
+    core = NexusCore(base_dir=DATABASE_DIR, read_only=1)
     core.close()
-    core = NexusCore(base_dir=DATABASE_DIR)
+    core = NexusCore(base_dir=DATABASE_DIR, read_only=1)
     start_time = time.time()
     total_saved = 0
     
@@ -83,83 +85,6 @@ def db_saver_process(data_queue, stop_event):
         now = datetime.now()
         formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
         print(f"[{formatted_time}][Process 1] DB 안전 종료 완료.")
-
-# def db_saver_process(data_queue, stop_event):
-#     now = datetime.now()
-#     formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
-#     print(f"[{formatted_time}][Process 1] DB Saver (Bulk Mode) 가동 중...")
-#     core = NexusCore(base_dir="/home/mizin/llm_info_db1")
-    
-#     start_time = time.time()
-#     total_saved = 0
-#     bulk_buffer = []  # 데이터를 모아둘 리스트
-#     BULK_SIZE = 100  # 100개씩 모아서 저장
-    
-#     try:
-#         while not (stop_event.is_set() and data_queue.empty()):
-#             try:
-#                 # 1. 큐에서 데이터 가져오기
-#                 data = data_queue.get(timeout=1)
-#                 url, content, description,q2_wait, p3_put_time = data
-                
-#                 if content:
-#                     bulk_buffer.append((url, content, description))
-#                 else:
-#                     print(f"[DB 저장 실패] content empty : {url}")
-                
-#                 # 2. 버퍼가 찼거나, 종료 신호가 왔을 때 저장
-#                 if len(bulk_buffer) >= BULK_SIZE or (stop_event.is_set() and not data_queue.empty()):
-#                     if bulk_buffer:
-#                         # NexusCore에 bulk_put 같은 메서드가 있다면 최적이지만, 
-#                         # 없다면 루프로 돌려도 내부적인 트랜잭션 처리가 효율적입니다.
-#                         for b_url, b_content, b_description in bulk_buffer:
-#                             if b_content:
-#                                 if b_description:
-#                                     metadata = {}
-#                                     metadata['description'] = b_description
-#                                     core.put(b_url, b_content,metadata=metadata)
-#                                 else:
-#                                     core.put(b_url, b_content)
-                        
-#                         total_saved += len(bulk_buffer)
-                        
-#                         # 통계 출력 (현재 시간당 속도 계산)
-#                         elapsed = time.time() - start_time
-#                         iph = (total_saved / elapsed) * 3600
-#                         now = datetime.now()
-#                         formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
-
-#                         print(f"[{formatted_time}] [BULK SAVE] {len(bulk_buffer)}개 저장 완료 | 누적: {total_saved} | 속도: {iph:.1f}/h")
-                        
-#                         bulk_buffer = [] # 버퍼 비우기
-
-#             except Empty:
-#                 # 큐가 비었더라도 버퍼에 남은 게 있다면 저장 (데이터 유실 방지)
-#                 if bulk_buffer:
-#                     for b_url, b_content, b_description in bulk_buffer:
-#                         if b_description:
-#                             metadata = {}
-#                             metadata['description'] = b_description
-#                             core.put(b_url, b_content,metadata=metadata)
-#                         else:
-#                             core.put(b_url, b_content)
-#                     total_saved += len(bulk_buffer)
-#                     bulk_buffer = []
-#                 continue
-#     finally:
-#         # 최종 남은 데이터 처리
-#         if bulk_buffer:
-#             for b_url, b_content, b_description in bulk_buffer:
-#                 if b_description:
-#                     metadata = {}
-#                     metadata['description'] = b_description
-#                     core.put(b_url, b_content,metadata=metadata)
-#                 else:
-#                     core.put(b_url, b_content)
-#         core.close()
-#         now = datetime.now()
-#         formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
-#         print(f"[{formatted_time}][Process 1] DB 안전 종료.")
 
 # --- 2번 프로세스용 함수들 ---
 def fetch_links(url):
@@ -218,7 +143,7 @@ def url_finder_process2(start_url, url_queue, stop_event):
                 # --- [추가] P2 잠시 멈춤 로직 (공급 조절) ---
                 # P3가 아직 처리하지 못한 URL이 큐에 500개 이상 쌓여있다면 대기
                 # 이 수치는 P3의 워커 수(50~100)의 5~10배 정도가 적당합니다.
-                if url_queue.qsize() > 200:
+                if url_queue.qsize() > 10:
                     # 큐가 비워질 때까지 2초씩 쉬면서 체크
                     # print(f"[P2] 대기 중... (현재 큐 잔량: {url_queue.qsize()}개)")
                     time.sleep(2)
@@ -253,7 +178,7 @@ def url_finder_process2(start_url, url_queue, stop_event):
         now = datetime.now()
         formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
         print(f"[{formatted_time}][Process 2] URL Finder 안전 종료.")
-        print(f"[{formatted_time}][Process 2] 남은 URL {url_queue.qsize()}개.")
+        print(f"[{formatted_time}][Process 2] 남은 URL {url_queue.qsize()+211}개.") 
 
 _SESSION = None
 
@@ -273,31 +198,19 @@ def get_session():
     return _SESSION
 
 # --- 3번 프로세스: 콘텐츠 정제 ---
-def process_content(url, p2_put_time, data_queue):
-    session = get_session()
+def process_content(url, p2_put_time):
+    # session = get_session()
     soup = None
-    html_text = ""
+    # html_text = ""
 
     try:
 
-        # 1. HEAD 요청으로 메타데이터만 확인 (매우 빠름)
-        # head_res = subprocess.run(
-        #     ['curl', '-I', '-L', '--connect-timeout', '2', '--max-time', '10', '-A', 'Mozilla/5.0', url],
-        #     capture_output=True, text=True, errors='ignore'
-        # )
-        # header = head_res.stdout.lower()
-        # date_info = None
-        # # HTML이 아니거나(예: 이미지), 용량이 너무 크면(예: 2MB 초과) 즉시 포기
-        # if "content-type: text/html" not in header:
-        #     return url, ""
-        # session = get_session()
-
-        # result = subprocess.run(
-        #     ['curl', '-s', '-L', '--max-time', '5', '-A', 'Mozilla/5.0', url],
-        #     capture_output=True, text=True, encoding='utf-8', errors='ignore'
-        # )
-        with session.get(url, timeout=5) as result:
-            html_text = result.text
+        result = subprocess.run(
+            ['curl', '-s', '-L', '--max-time', '5', '-A', 'Mozilla/5.0', url],
+            capture_output=True, text=True, encoding='utf-8', errors='ignore'
+        )
+        # with session.get(url, timeout=5) as result:
+            # html_text = result.text
         # if result.status_code != 200: 
         #     print("wtf")
         #     return url, "", ""
@@ -305,8 +218,8 @@ def process_content(url, p2_put_time, data_queue):
             # return url, "", ""
 
 
-        soup = BeautifulSoup(html_text, 'lxml')
-        # soup = BeautifulSoup(result.stdout, 'lxml')
+        # soup = BeautifulSoup(html_text, 'lxml')
+        soup = BeautifulSoup(result.stdout, 'lxml')
         
         final_description = ""
         final_content = ""
@@ -340,7 +253,8 @@ def process_content(url, p2_put_time, data_queue):
 
         if not final_content:
             final_content = trafilatura.extract(
-            html_text, 
+            # html_text, 
+            result.stdout,
             no_fallback=True, 
             include_comments=False, 
             include_tables=False
@@ -395,17 +309,14 @@ def process_content(url, p2_put_time, data_queue):
         if refine_date is not None and refine_date: metadata['publish_date'] = refine_date
         data_to_send_metadata = metadata if metadata else None
 
-        data_queue.put((url, refined_content, data_to_send_metadata,time.time() - p2_put_time, time.time()))
-        # return url, refined_content, refined_description
-        # global processed_count
-        # processed_count += 1
-        # print(processed_count)
-        return 1
+        # data_queue.put((url, refined_content, data_to_send_metadata,time.time() - p2_put_time, time.time()))
+        return url, refined_content, data_to_send_metadata,time.time() - p2_put_time, time.time()
+        # return 
     except Exception as e:
         # [중요] 예외 객체를 즉시 날려서 Traceback이 지역 변수를 붙잡지 못하게 함
         # print(f"[process 3]{e}")
         e = None 
-        return 0#url, "", ""
+        return #url, "", ""
     finally:
         # 3. [핵심] BeautifulSoup 트리의 순환 참조를 강제로 끊어 메모리 폭발 방지
         if soup:
@@ -414,77 +325,111 @@ def process_content(url, p2_put_time, data_queue):
         # 4. 남아있는 대용량 변수 명시적 삭제
         html_text = None
         soup = None
-import tracemalloc
+# import tracemalloc
 def content_extractor_process(url_queue, data_queue, stop_event):
     now = datetime.now()
     formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
     print(f"[{formatted_time}][Process 3] Content Extractor 가동 중...")
 
         # 메모리 추적 시작
-    tracemalloc.start()
+    # tracemalloc.start()
     max_queue_size = int(CONTETN_EXTRACT_PROCESS_WORKER * 1.2)
-    semaphore = threading.Semaphore(max_queue_size)
+    # semaphore = threading.Semaphore(max_queue_size)
+    # processed_count = 1
 
-    processed_count = 1
+    # with ThreadPoolExecutor(max_workers=CONTETN_EXTRACT_PROCESS_WORKER,max_tasks_per_child=100) as executor:
+    #     while True: #not (stop_event.is_set() and url_queue.empty()):
+    #         try:
+    #             # 큐에서 (url, p2_put_time) 꺼냄
+    #             target_url, p2_put_time = url_queue.get(timeout=1)
+    #             # q2_wait_time = time.time() - p2_put_time
+                
+    #             future = executor.submit(process_content, target_url, p2_put_time, data_queue)
+    #             # executor.submit(process_content, target_url, p2_put_time, data_queue)
+                
+    #             processed_count += 1
+    #             semaphore.acquire()
+                
+    #             # if processed_count % 1001 == 0 or processed_count % 1002 == 0 or processed_count % 1003 == 0 or processed_count % 1004 == 0 or processed_count % 1005 == 0 or processed_count % 1006 == 0 or processed_count % 1007 == 0 or processed_count % 1008 == 0 or processed_count % 1009 == 0:
+    #             #     # 2. Process 3 내부에서 캐시 및 GC 청소 (여기서 해야 의미가 있습니다!)
+    #             #     # reset_caches()
+    #             #     # gc.collect()
+    #             #     # 1. 범인 색출 로직
+    #             #     snapshot = tracemalloc.take_snapshot()
+    #             #     top_stats = snapshot.statistics('lineno')
+    #             #     print("\n" + "="*50)
+    #             #     print(f"[Process 3 메모리 누수 추적 - Top 5]")
+    #             #     for stat in top_stats[:19]:
+    #             #         print(stat)
+    #             #     print("="*50 + "\n")
+    #             # print(processed_count)
+    #             if processed_count >= 1000:
+    #                 reset_caches()
+    #                 gc.collect()
+    #                 now = datetime.now()
+    #                 formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    #                 print(f"[{formatted_time}][System] 메모리 최적화", flush=True)
+    #                 processed_count = 0
+    #                 # 1. 범인 색출 로직
+    #                 snapshot = tracemalloc.take_snapshot()
+    #                 top_stats = snapshot.statistics('lineno')
+    #                 print("\n" + "="*50)
+    #                 print(f"[Process 3 메모리 누수 추적 - Top 5]")
+    #                 for stat in top_stats[:19]:
+    #                     print(stat)
+    #                 print("="*50 + "\n")
 
-    with ThreadPoolExecutor(max_workers=CONTETN_EXTRACT_PROCESS_WORKER,max_tasks_per_child=100) as executor:
-        while True: #not (stop_event.is_set() and url_queue.empty()):
-            try:
-                # 큐에서 (url, p2_put_time) 꺼냄
-                target_url, p2_put_time = url_queue.get(timeout=1)
-                # q2_wait_time = time.time() - p2_put_time
-                
-                future = executor.submit(process_content, target_url, p2_put_time, data_queue)
-                # executor.submit(process_content, target_url, p2_put_time, data_queue)
-                
-                processed_count += 1
-                semaphore.acquire()
-                
-                # if processed_count % 1001 == 0 or processed_count % 1002 == 0 or processed_count % 1003 == 0 or processed_count % 1004 == 0 or processed_count % 1005 == 0 or processed_count % 1006 == 0 or processed_count % 1007 == 0 or processed_count % 1008 == 0 or processed_count % 1009 == 0:
-                #     # 2. Process 3 내부에서 캐시 및 GC 청소 (여기서 해야 의미가 있습니다!)
-                #     # reset_caches()
-                #     # gc.collect()
-                #     # 1. 범인 색출 로직
-                #     snapshot = tracemalloc.take_snapshot()
-                #     top_stats = snapshot.statistics('lineno')
-                #     print("\n" + "="*50)
-                #     print(f"[Process 3 메모리 누수 추적 - Top 5]")
-                #     for stat in top_stats[:19]:
-                #         print(stat)
-                #     print("="*50 + "\n")
-                # print(processed_count)
-                if processed_count >= 1000:
-                    reset_caches()
-                    gc.collect()
-                    now = datetime.now()
-                    formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
-                    print(f"[{formatted_time}][System] 메모리 최적화", flush=True)
-                    processed_count = 0
-                    # 1. 범인 색출 로직
-                    snapshot = tracemalloc.take_snapshot()
-                    top_stats = snapshot.statistics('lineno')
-                    print("\n" + "="*50)
-                    print(f"[Process 3 메모리 누수 추적 - Top 5]")
-                    for stat in top_stats[:19]:
-                        print(stat)
-                    print("="*50 + "\n")
-
-                def done_callback(fut):
-                    num = fut.result()
-                    #processed_count += num
+    #             def done_callback(fut):
+    #                 num = fut.result()
+    #                 #processed_count += num
                     
-                    semaphore.release()
-                    # (url, content, P2대기시간, P3넣은시간)
-                    # data_queue.put((res_url, res_content, res_description,q2_wait, time.time()))
+    #                 semaphore.release()
+    #                 # (url, content, P2대기시간, P3넣은시간)
+    #                 # data_queue.put((res_url, res_content, res_description,q2_wait, time.time()))
                 
-                future.add_done_callback(done_callback)
+    #             future.add_done_callback(done_callback)
+    #         except Empty:
+    #             if stop_event.is_set(): break
+    #             continue
+    #         except Exception as e:
+    #             print(f"에러 발생: {e}", flush=True)
+
+    def done_callback(result):
+        if result: # 워커에서 에러 없이 정상 리턴된 경우
+            data_queue.put(result)
+            
+    def error_callback(e):
+        print(e)
+        pass # 필요 시 에러 로깅
+
+    with multiprocessing.Pool(processes=CONTETN_EXTRACT_PROCESS_WORKER, maxtasksperchild=MAXWORKPERCHILD) as pool:
+        while not (stop_event.is_set() and pool._taskqueue.qsize()==0):
+            try:
+                target_url, p2_put_time = url_queue.get(timeout=1)
+                if pool._taskqueue.qsize() > 200:
+                    time.sleep(2)
+                    continue
+                # 비동기로 Pool에 작업 던지기
+                pool.apply_async(
+                    func=process_content,
+                    args=(target_url, p2_put_time),
+                    callback=done_callback,
+                    error_callback=error_callback
+                )
             except Empty:
                 if stop_event.is_set(): break
                 continue
             except Exception as e:
                 print(f"에러 발생: {e}", flush=True)
-    data_queue.put(None)
-    print("[Process 3] Content Extractor 안전 종료.")
+                
+        # 종료 신호가 오면 더 이상 작업을 받지 않고 대기
+        pool.close()
+        pool.join()
+        data_queue.put(None)
+        now = datetime.now()
+        formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        print(f"\n[{formatted_time}][Process 3] Content Extractor 안전 종료.")
+        
 
 def signal_handler(sig, frame, stop_event):
     now = datetime.now()
